553fc9e1eab32207c2faa772104748c217b3a64f
mariquenaallosa <allosamariquena@gmail.com>
2022-07-29 15:46:45 -0300

    Repository

--- src/main/java/com/portfolio/mariquena/Repository/REducacion.java
+++ src/main/java/com/portfolio/mariquena/Repository/REducacion.java
@@ -3,8 +3,10 @@ package com.portfolio.mariquena.Repository;
 import com.portfolio.mariquena.Entity.Educacion;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface REducacion extends JpaRepository<Educacion, Integer> {
     Optional<Educacion> findByTituloEd(String tituloEd);
-    boolean existsByTituloEd(String tituloEd);
+    public boolean existsByTituloEd(String tituloEd);
 }
